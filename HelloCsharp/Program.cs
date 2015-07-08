742db8b64f6a524f56dbe7f5988bdf76f9c41f71
padeoe <padeoe@gmail.com>
2015-07-08 23:45:50 +0800

    initial commit

---
+++ HelloCsharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// This is my first C# program!
+/// </summary>
+namespace HelloCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,C#!");
+        }
+    }
+}
